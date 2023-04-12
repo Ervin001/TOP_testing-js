6a9b3a8c6506df1f26fc3e98fc5cc53b1fd815ca
Ervin <evgnz24@gmail.com>
2023-04-12 10:06:14 -0500

    First commit. Set up file

---
+++ main.js
@@ -0,0 +1,9 @@
+// capitalize: returns a string with a cap
+
+// reverseString: returns a reversed string
+
+// calculator: object that contains functions for the basic operations Add, Sub, Multiply, Divide
+
+// caesarCipher: string ciphered by a shift factor
+
+// analyzeArray: function that takes an array of numbers and returns an object with the following properties
